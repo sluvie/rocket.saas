8cb203de7940b52f9ac6342f92596a228613a5e5
Sulianto Suhono <sluvie@gmail.com>
2022-07-04 09:42:03 +0700

    flask_wtf

--- app/forms/user.py
+++ app/forms/user.py
@@ -1,7 +1,8 @@
-from flask.ext.wtf import Form
+from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import (Required, Length, Email, ValidationError,
                                 EqualTo)
 
 class Login(Form):
     email = TextField(validators=[Required(), Email()], description="Email address")
+    
