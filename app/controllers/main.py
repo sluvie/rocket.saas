af93adbe6314973e39e7d8457763dc8f993e123c
sluvie <sulianto.suhono@gmail.com>
2022-07-14 12:25:51 +0700

    signin signup function

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -11,4 +11,9 @@ from app import app
 @app.route('/', methods = ['GET'])
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="rocket.saas")
+
+
+@app.route('/home', methods = ['GET'])
+def home():
+    return render_template('home.html', title="main menu")
--- app/controllers/user.py
+++ app/controllers/user.py
@@ -5,7 +5,8 @@ from flask import (
     request,
     session,
     redirect,
-    url_for
+    url_for,
+    flash
 )
 from app import app
 from app.forms import user as user_forms
@@ -18,7 +19,9 @@ userbp = Blueprint('userbp', __name__, url_prefix='/user')
 
 @userbp.route('/signup', methods=['GET', 'POST'])
 def signup():
-    form = user_forms.SignUp()
+    form = user_forms.SignUp(request.form)
+
+    # form submit
     if request.method == 'POST' and form.validate():
         return redirect(url_for('index'))
 
@@ -27,5 +30,18 @@ def signup():
 
 @userbp.route('/signin', methods=['GET', 'POST'])
 def signin():
-    form = user_forms.SignIn()
+    form = user_forms.SignIn(request.form)
+
+    # initialize sample input
+    if request.method=="GET":
+        form.email.data = "demo@rocket.com"
+
+    # form submit
+    if request.method=="POST" and form.validate():
+        if form.email.data == "demo@rocket.com" and form.email.data == "demodemo":
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid account.', 'negative')
+            return redirect(url_for('userbp.signin'))
+
     return render_template('user/signin.html', form=form, title='Sign In')
